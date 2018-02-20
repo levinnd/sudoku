e52a19d0a321a8bc0b94c6d803cdf13012f61644
Nate <levinnd@gmail.com>
2018-02-20 00:08:59 -0500

    now the code will run from a jar file or from eclipse

--- sudoku/src/driver/Driver.java
+++ sudoku/src/driver/Driver.java
@@ -1,6 +1,8 @@
 
 package driver;
 
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -18,20 +20,43 @@ public class Driver extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("../resources/MainUI.fxml"));
-		Parent root = loader.load();
-		controller = loader.getController();
+
+		FXMLLoader loader;
+
+		URL url = Driver.class.getResource("Driver.class");
 		
+		if(url.toString().startsWith("jar:")){
+			
+			loader = new FXMLLoader();
+
+			String relativePath = "/MainUI.fxml";
+
+			URL uri = Driver.class.getResource(relativePath);
+
+			loader.setLocation(uri);
+			
+		}
+
+		else{	
+			
+			loader = new FXMLLoader(getClass().getResource("../resources/MainUI.fxml"));
+
+		}
+
+		Parent root = loader.load();   
+
+		controller = loader.getController();
+
 		stage.setTitle("Sudoku Master");
 		stage.setScene(new Scene(root));
 		stage.show();
-        stage.setMinWidth(stage.getWidth());
-        stage.setMinHeight(stage.getHeight());
-		
+		stage.setMinWidth(stage.getWidth());
+		stage.setMinHeight(stage.getHeight());
+
 		controller.initialize(stage);
 	}
 
-	
+
 
 }
 
